fc1466c7315ba3adf7b853dd56c3fb558dbf5a94
Ederas229 <120315229+Ederas229@users.noreply.github.com>
2022-12-28 21:07:39 +0100

    Format document

--- scripts/module.js
+++ scripts/module.js
@@ -1,42 +1,42 @@
 Hooks.once("setup", () => {
     game.modules.get("pf2e-eidolon-sync").api = {
-      setSync,
-      removeSync,
-      setMaxHp
+        setSync,
+        removeSync,
+        setMaxHp
     }
 })
 
-Hooks.on('preUpdateActor', async function(character, change) {
+Hooks.on('preUpdateActor', async function (character, change) {
 
     const syncActorUuid = character.getFlag("pf2e-eidolon-sync", "syncActor");
-    if (syncActorUuid){
+    if (syncActorUuid) {
         const syncActor = fromUuidSync(syncActorUuid);
 
         syncHeroPoints(syncActor, change);
-        if (character.getFlag("pf2e-eidolon-sync", "onlyHero")){return;}
+        if (character.getFlag("pf2e-eidolon-sync", "onlyHero")) { return; }
         syncHp(syncActor, change);
     }
 });
 
-function syncHeroPoints(syncActor, change){
-    if (!change.system?.resources?.heroPoints){return;}
+function syncHeroPoints(syncActor, change) {
+    if (!change.system?.resources?.heroPoints) { return; }
     const update = syncActor.system.resources.heroPoints;
     update.value = change.system.resources.heroPoints.value;
-    syncActor.update({"system.resources.heroPoints":update}, {"noHook":true});
+    syncActor.update({ "system.resources.heroPoints": update }, { "noHook": true });
 }
 
-function syncHp(syncActor, change){
+function syncHp(syncActor, change) {
 
-    if (!change.system?.attributes?.hp){return;}
+    if (!change.system?.attributes?.hp) { return; }
 
     const update = syncActor.system.attributes.hp;
     update.value = change.system.attributes.hp.value;
     update.temp = change.system.attributes.hp.temp;
-    syncActor.update({"system.attributes.hp":update}, {"noHook":true});
-    
+    syncActor.update({ "system.attributes.hp": update }, { "noHook": true });
+
 }
 
-export async function setMaxHp(actor){
+export async function setMaxHp(actor) {
 
     const id = actor.itemTypes.effect.find(e => e.name === 'Sync hp')?.id;
 
@@ -56,18 +56,18 @@ export async function setMaxHp(actor){
     syncHp(actor, syncActor);
 }
 
-export function setSync(character, syncActor, onlyHero){
+export function setSync(character, syncActor, onlyHero) {
     character.setFlag("pf2e-eidolon-sync", "syncActor", syncActor.uuid);
     syncActor.setFlag("pf2e-eidolon-sync", "syncActor", character.uuid);
-    ui.notifications.info("Synchronisation added between "+character.name+" and "+syncActor.name);
+    ui.notifications.info("Synchronisation added between " + character.name + " and " + syncActor.name);
 
 }
 
-export function removeSync(character){
+export function removeSync(character) {
     const syncActorUuid = character.getFlag("pf2e-eidolon-sync", "syncActor");
-    if (!syncActorUuid){return;}
+    if (!syncActorUuid) { return; }
     character.unsetFlag("pf2e-eidolon-sync", "syncActor");
     const syncActor = fromUuidSync(syncActorUuid);
     syncActor.unsetFlag("pf2e-eidolon-sync", "syncActor");
-    ui.notifications.info("Synchronisation removed between "+character.name+" and "+syncActor.name);
+    ui.notifications.info("Synchronisation removed between " + character.name + " and " + syncActor.name);
 }
